0781034915c093224e804287da71966e5dc5a01b
KevinLong <long.luong.contact@gmail.com>
2026-04-16 12:53:54 +0700

    Fix path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
+const githubPagesBase =
+  "/Final-Project---Luong-Dieu-Long--Thai-Duong-Son---PNL-CIJS110---Nhom-1/";
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/Final-Project---Luong-Dieu-Long--Thai-Duong-Son---PNL-CIJS110---Nhom-1/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? githubPagesBase : "/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-});
+}));
